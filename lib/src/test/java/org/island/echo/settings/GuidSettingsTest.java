729c349bbd84e498922723d593663116f999241e
Sean <seanbatten58@gmail.com>
2023-05-18 10:34:18 +0100

    New test

--- lib/src/test/java/org/island/echo/settings/GuidSettingsTest.java
+++ lib/src/test/java/org/island/echo/settings/GuidSettingsTest.java
@@ -1,4 +1,37 @@
 package org.island.echo.settings;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class GuidSettingsTest {
+    @Test
+    public void unknownSetting() {
+        var setting = new GuidSettings();
+        var value = setting.getSetting("foo");
+        assertFalse(value.isPresent());
+    }
+
+    @Test
+    public void appGuidStaysTheSame() {
+        var setting = new GuidSettings();
+        var value = setting.getSetting("app");
+        assertTrue(value.isPresent());
+        assertNotNull(value.get());
+
+        var secondTime = setting.getSetting("app");
+        assertTrue(secondTime.isPresent());
+        assertEquals(value.get(), secondTime.get());
+    }
+
+    @Test
+    public void newGuidStaysChangesd() {
+        var setting = new GuidSettings();
+        var value = setting.getSetting("new");
+        assertTrue(value.isPresent());
+        assertNotNull(value.get());
+
+        var secondTime = setting.getSetting("new");
+        assertTrue(secondTime.isPresent());
+        assertNotEquals(value.get(), secondTime.get());
+    }
 }
